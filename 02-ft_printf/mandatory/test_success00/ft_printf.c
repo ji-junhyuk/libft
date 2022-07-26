2a0c564e32fa64c05cfc69ae01eb0fe65e72e6db
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-07-26 15:55:56 +0900

    norm

--- 02-ft_printf/mandatory/test_success00/ft_printf.c
+++ 02-ft_printf/mandatory/test_success00/ft_printf.c
@@ -1,5 +1,16 @@
-#include <stdarg.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junji <junji@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 15:16:23 by junji             #+#    #+#             */
+/*   Updated: 2022/07/26 15:52:58 by junji            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
 
 int	find_len(int num)
 {
@@ -51,11 +62,15 @@ int	ft_putnbr_fd(long long n, int fd, int *printed)
 int	print_sm_hex(va_list *ap, int *printed)
 {
 	unsigned int			value;
-	static char				c[9];
-	static const char		*hex = "0123456789abcdef";
+	char					c[9];
+	const char				*hex = "0123456789abcdef";
+	int						idx;
 
 	value = va_arg(*ap, unsigned int);
-	int idx = -1;
+	idx = -1;
+	while (++idx < 9)
+		c[idx] = 0;
+	idx = -1;
 	while (1)
 	{
 		c[++idx] = hex[value % 16];
@@ -74,12 +89,16 @@ int	print_sm_hex(va_list *ap, int *printed)
 
 int	print_bg_hex(va_list *ap, int *printed)
 {
-	unsigned int value;
-	static char 	c[9];
-	value = va_arg(*ap, unsigned int);
-	char *hex = "0123456789ABCDEF";
+	const char		*hex = "0123456789ABCDEF";
+	unsigned int	value;
+	char			c[9];
+	int				idx;
 
-	int idx = -1;
+	idx = -1;
+	while (++idx < 9)
+		c[idx] = 0;
+	value = va_arg(*ap, unsigned int);
+	idx = -1;
 	while (1)
 	{
 		c[++idx] = hex[value % 16];
@@ -107,6 +126,7 @@ int	print_per(va_list *ap, int *printed)
 int	print_dec(va_list *ap, int *printed)
 {
 	int	value;
+
 	value = va_arg(*ap, int);
 	if (ft_putnbr_fd(value, STDOUT_FILENO, printed) == -1)
 		return (-1);
@@ -115,7 +135,7 @@ int	print_dec(va_list *ap, int *printed)
 
 int	print_un_dec(va_list *ap, int *printed)
 {
-	unsigned int value;
+	unsigned int	value;
 
 	value = va_arg(*ap, unsigned int);
 	if (ft_putnbr_fd(value, STDOUT_FILENO, printed) == -1)
@@ -125,10 +145,10 @@ int	print_un_dec(va_list *ap, int *printed)
 
 int	print_addr(void *ptr, int *printed)
 {
-	unsigned long long value;
-	char	addr[16];
-	char	*hex = "0123456789abcdef";
-	int		cnt;
+	const char			*hex = "0123456789abcdef";
+	unsigned long long	value;
+	char				addr[16];
+	int					cnt;
 
 	value = (unsigned long long)ptr;
 	if (write(1, "0x", 2) == -1)
@@ -153,7 +173,7 @@ int	print_addr(void *ptr, int *printed)
 
 int	print_ptr(va_list *ap, int *printed)
 {
-	void *p;
+	void	*p;
 
 	p = va_arg(*ap, void *);
 	print_addr(p, printed);
@@ -166,7 +186,7 @@ int	print_ptr(va_list *ap, int *printed)
 	return (*printed);
 }
 
-int 	ft_putstr(unsigned char *str, int *printed)
+int	ft_putstr(unsigned char *str, int *printed)
 {
 	if (!str)
 	{
@@ -185,7 +205,7 @@ int 	ft_putstr(unsigned char *str, int *printed)
 
 int	print_str(va_list *ap, int *printed)
 {
-	unsigned char *value;
+	unsigned char	*value;
 
 	value = va_arg(*ap, unsigned char *);
 	if (ft_putstr(value, printed) == -1)
@@ -221,12 +241,12 @@ void	init_funcs(void *funcs[])
 	funcs['%'] = print_per;
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	va_list ap;
-	void	*funcs[256];
-	int		(*func)(va_list *, int *);
-	int		printed;
+	va_list		ap;
+	void		*funcs[256];
+	int			(*func)(va_list *, int *);
+	int			printed;
 
 	printed = 0;
 	init_funcs(funcs);
@@ -242,61 +262,10 @@ int ft_printf(const char *format, ...)
 		else
 		{
 			func = funcs[(int)(*++format)];
-			if (func || (func(&ap, &printed) == -1))
+			if (!func || (func(&ap, &printed) == -1))
 				return (-1);
 			++format;
 		}
 	}
 	return (printed);
-}
-//
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void)
-{
-	int num;
-	num = 210;
-//	printf("%d\n",	ft_printf("junto%d\n", 123));
-//	ft_printf("%d\n",	ft_printf("junto%d\n", 123));
-//	printf("%u\n",	ft_printf("junto%u\n", 2147483649));
-//	printf("%u\n",	ft_printf("junto%u\n", 2147483649));
-//	printf("p:%p\n", &num);
-//	ft_printf("ft_printf]:%p\n", &num);
-//	printf("%x\n", num);
-//	ft_printf("%x\n", num);
-//	printf("%X\n", num);
-//	ft_printf("%X\n", num);
-//	ft_printf("junto%i\n", 123);
-//	ft_printf("junto%%\n", 123);
-
-//	ft_printf("%s", "");
-//	ft_printf(" %s\n", "");
-//	ft_printf("%s \n", "");
-//	ft_printf(" %s ", "");
-//	ft_printf(" %s ", "-");
-//	ft_printf(" %s %s ", "", "-");
-//	ft_printf(" %s %s ", " - ", "");
-//	ft_printf(" %s %s %s %s %s", " - ", "", "4", "", s2);
-//	ft_printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
-//	ft_printf("%d\n", printf("%s", NULL));
-//	ft_printf(" %p %p \n", (void *)0, (void *)0);
-//	printf(" %p %p \n", (void *)0, (void *)0);
-//	printf("%s", "");
-	ft_printf("%x\n", 314748648);
-	printf("%x\n", 314748648);
-
-	printf("%v123123\n");
-	ft_printf("%m\n");
-//	printf(" %s\n", "");
-//	printf("%s \n", "");
-//	printf(" %s ", "");
-//	printf(" %s ", "-");
-//	printf(" %s %s ", "", "-");
-//	printf(" %s %s ", " - ", "");
-//	printf(" %s %s %s %s %s", " - ", "", "4", "", s2);
-//	printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
-//	printf("%d\n", printf("%s", NULL));
 }
