82cff322aded170a42eef9f8d9f11ba1d318d17b
junto <67992469+ji-junhyuk@users.noreply.github.com>
2022-10-20 15:59:16 +0900

    Update get_next_line.h

--- 10-get_next_line/2st_try_get_next_line/get_next_line.h
+++ 10-get_next_line/2st_try_get_next_line/get_next_line.h
@@ -12,21 +12,16 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# if BUFFER_SIZE < 1
-#  error invalid BUFFER_SIZE
-# endif
+# define BUFFER_SIZE 42
 
 # include <stdlib.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
-void	ft_strcat(char *dest, const char *src);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_strcpy(char *dest, const char *src);
 char	*ft_strdup(const char *src);
 
 #endif
